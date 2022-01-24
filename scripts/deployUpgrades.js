1596b96bbbce77f283bd9633b9bd7e3b73f52926
Jason Harrington <jason.m.harrington@gmail.com>
2022-01-23 22:40:33 -0500

    Add deployUpgrades.js sketch

---
+++ scripts/deployUpgrades.js
@@ -0,0 +1,78 @@
+const TICK = 1000;
+
+/** @param {NS} ns **/
+export async function main(ns) {
+	await deployUpgrades(ns);
+}
+
+async function deployUpgrades(ns) {
+	while(upgradesAvailable(ns) || nodesDeploying(ns)) {
+		await buyUpgrades(ns);
+	}
+}
+
+function upgradesAvailable(ns) {
+	return;
+}
+
+function nodesDeploying (ns) { return ns.isRunning(`/scripts/autoHacknet.js`) }
+
+async function buyUpgrades(ns){
+	return;
+}
+
+/*
+
+async function upgradeNode(ns, node) {
+	ns.disableLog('sleep');
+	ns.disableLog('getServerMoneyAvailable');
+
+	ns.tprint(`Upgrading hacknet-node-${node}`);
+	while (upgradesAvailable(ns, node)) {
+		let [upgrade, cost] = leastCostlyUpgrade(ns, node);
+		ns.print(`Least costly upgrade for hacknet-node-${node} is ${upgrade} at $${cost}`);
+		while (!affordable(ns, cost)) { await ns.sleep(TICK); }
+		buyUpgrade(ns, node, upgrade);
+	}
+	ns.tprint(`hacknet-node-${node} fully upgraded`);
+}
+
+function upgradesAvailable(ns, node) { return (levelAvailable(ns, node) || ramAvailable(ns, node) || coreAvailable(ns, node)) ? true : false; }
+
+function levelAvailable(ns, node) { return isFinite(cost(ns, node, 'level')) ? true : false; }
+function ramAvailable(ns, node) { return isFinite(cost(ns, node, 'ram')) ? true : false; }
+function coreAvailable(ns, node) { return isFinite(cost(ns, node, 'core')) ? true : false; }
+
+function leastCostlyUpgrade(ns, node) {
+	let minCost = cost(ns, node, 'level');
+	let upgrade = 'level';
+
+	let ramCost = cost(ns, node, 'ram');
+	if (ramCost < minCost) {
+		minCost = ramCost;
+		upgrade = 'ram';
+	}
+
+	var coreCost = cost(ns, node, 'core');
+	if (coreCost < minCost) {
+		minCost = coreCost;
+		upgrade = 'core';
+	}
+
+	return isFinite(minCost) ? [upgrade, minCost] : [null, null];
+}
+
+function cost(ns, node, upgrade) {
+	let part = upgrade.charAt(0).toUpperCase() + upgrade.slice(1);
+	let costFunction = `get${part}UpgradeCost`;
+	return ns.hacknet[costFunction](node, 1);
+}
+
+function affordable(ns, cost) { return cost < moneyAvailable(ns) ? true : false; }
+function moneyAvailable(ns) { return ns.getServerMoneyAvailable('home'); }
+
+function buyUpgrade(ns, node, upgrade) {
+	let part = upgrade.charAt(0).toUpperCase() + upgrade.slice(1);
+	let upgradeFunction = `upgrade${part}`;
+	return ns.hacknet[upgradeFunction](node, 1);
+}
