14c211c7c33b7e775873d845ea888627ea3afda8
J Harrington <jason.m.harrington@gmail.com>
2022-02-26 22:52:11 -0500

    Add recruitment and begin task management

--- scripts/gangManager.js
+++ scripts/gangManager.js
@@ -1,5 +1,5 @@
 const ATTRIBUTES = ['hack', 'str', 'def', 'dex', 'agi', 'cha'];
-const TICK = 5000;
+const TICK = 10 * 1000;
 
 /** @param {NS} ns **/
 export async function main(ns) {
@@ -7,40 +7,68 @@ export async function main(ns) {
   ns.tail();
 
   while (true) {
-    let members = ns.gang.getMemberNames();
-    for (let member of members) {
-      let memberInfo = ns.gang.getMemberInformation(member);
-
-      let ascendResult = ns.gang.getAscensionResult(member);
-      if (typeof ascendResult === 'undefined') { continue; }
-
-      let attrSnaps = [];
-      for (let attribute of ATTRIBUTES) {
-        let expString = `${attribute}_exp`;
-        let baseMultString = `${attribute}_mult`;
-        let ascMultString = `${attribute}_asc_mult`;
-        let attrSnap = {
-          'name': attribute,
-          'rank': memberInfo[attribute],
-          'xp': memberInfo[expString],
-          'baseMult': memberInfo[baseMultString],
-          'ascMult': memberInfo[ascMultString],
-          'ascResult': ascendResult[attribute],
-          'ascGain': memberInfo[ascMultString] * (ascendResult[attribute] - 1),
-        };
-        attrSnaps.unshift(attrSnap);
-      }
-
-      let growthFocus = attrSnaps.sort((a, b) => b.xp - a.xp)[0];
-      // ns.print(`${memberInfo.name} -> ${growthFocus.name} -> ${growthFocus.ascGain}`);
-
-      if (growthFocus.ascGain > ascGainTarget(growthFocus)) { ns.gang.ascendMember(memberInfo.name) };
-    }
+    recruitment(ns);
+    ascension(ns);
     await ns.sleep(TICK);
   }
 }
 
-function ascGainTarget(attrs) {
-  // return attrs.ascGain * Math.log(attrs.xp);
-  return 2;
+function recruitment(ns) {
+  if (!canRecruit(ns)) { return false; }
+  let name = getRecruitName();
+  return (recruit(ns, name) && setTask(ns, name, `Train Hacking`));
+}
+
+function setTask(ns, name, task) { return ns.gang.setMemberTask(name, task); }
+function recruit(ns, name) { return ns.gang.recruitMember(name); }
+function canRecruit(ns) { return ns.gang.canRecruitMember(); }
+function getRecruitName() { return Math.random().toString(16).substring(2, 8); }
+
+function getGangData(ns) {
+  let members = ns.gang.getMemberNames();
+  for (let member of members) {
+
+  }
+}
+
+function ascension(ns) {
+  let members = ns.gang.getMemberNames();
+  for (let member of members) {
+    let memberInfo = ns.gang.getMemberInformation(member);
+
+    let ascendResult = ns.gang.getAscensionResult(member);
+    if (typeof ascendResult === 'undefined') { continue; }
+
+    let attrSnaps = [];
+    for (let attribute of ATTRIBUTES) {
+      let expString = `${attribute}_exp`;
+      let baseMultString = `${attribute}_mult`;
+      let ascMultString = `${attribute}_asc_mult`;
+      let attrSnap = {
+        'name': attribute,
+        'rank': memberInfo[attribute],
+        'xp': memberInfo[expString],
+        'baseMult': memberInfo[baseMultString],
+        'ascMult': memberInfo[ascMultString],
+        'ascResult': ascendResult[attribute],
+        'ascGain': memberInfo[ascMultString] * (ascendResult[attribute] - 1),
+      };
+      attrSnaps.unshift(attrSnap);
+    }
+
+    let growthFocus = attrSnaps.sort((a, b) => b.xp - a.xp)[0];
+    ns.print(`${memberInfo.name}.${growthFocus.name}: ${growthFocus.ascGain - ascGainTarget(growthFocus)}`);
+    if (growthFocus.ascGain > ascGainTarget(growthFocus)) {
+      ns.gang.ascendMember(memberInfo.name)
+    }
+  }
+}
+
+function ascGainTarget(attributeData) { return (Math.log10(attributeData.ascMult) > 1) ? Math.log10(attributeData.ascMult): 1; }
+
+function workAssignment(ns) {
+  let members = [];
+  for (let member of ns.gang.getMemberNames()) {
+    members.push(ns.gang.getMemberInformation(member))
+  }
 }
