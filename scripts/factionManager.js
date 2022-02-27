cfcbdaa1b23cd3c25f1eecd676863b283ba168c9
J Harrington <jason.m.harrington@gmail.com>
2022-02-26 22:50:15 -0500

    Refactor get aug data

--- scripts/factionManager.js
+++ scripts/factionManager.js
@@ -1,26 +1,28 @@
-const FACTIONS = [`NiteSec`, `Aevum`];
+const FACTIONS = [`Netburners`, `CyberSec`];
 const WORK = `Hacking Contracts`;
 const TICK = 60 * 1000;
 
 /** @param {NS} ns **/
 export async function main(ns) {
 	for (let faction of FACTIONS) {
-		let augmentationData = [];
-		let augmentations = ns.getAugmentationsFromFaction(faction);
-		for (let augmentation of augmentations) {
-			augmentationData.push({
-				'name': augmentation,
-				'price': ns.getAugmentationPrice(augmentation),
-				'rep': ns.getAugmentationRepReq(augmentation),
-			});
-		}
-		let maxRep = augmentationData.sort((a, b) => b.rep - a.rep)[0].rep;
-		ns.tprint(maxRep);
+		let maxRep = getAugmentationData(ns, faction).sort((a, b) => b.rep - a.rep)[0].rep;
 		while (ns.getFactionRep(faction) < maxRep) {
 			ns.workForFaction(faction, WORK, false);
 			await ns.sleep(TICK);
 		}
 		ns.stopAction();
 		ns.tprint(`Sufficient Rep: ${faction}`);
-	};
-};
+	}
+}
+
+function getAugmentationData(ns, faction) {
+	let augmentationData = [];
+	for (let augmentation of ns.getAugmentationsFromFaction(faction)) {
+		augmentationData.push({
+			'name': augmentation,
+			'price': ns.getAugmentationPrice(augmentation),
+			'rep': ns.getAugmentationRepReq(augmentation),
+		});
+	}
+	return augmentationData;
+}
