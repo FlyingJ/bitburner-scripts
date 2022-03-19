173ad8e06a0c5c89d8b19b53a6e3888bd72b285d
J Harrington <jason.m.harrington@gmail.com>
2022-03-19 13:50:04 -0400

    Add warehouse expansion and production boost resource purchasing to officeManager

--- bin/officeManager.js
+++ bin/officeManager.js
@@ -1,4 +1,4 @@
-const city = `New Tokyo`;
+const city = `Aevum`;
 const industry = `Agriculture`;
 const headCount = 33;
 const employeeDistribution = {
@@ -8,41 +8,51 @@ const employeeDistribution = {
 	'Management': 1,
 	'Research & Development': 5,
 };
-const warehouseSize = 56 * 1000;
-const realEstateBuyVolume = 10 * 1000 * 1000;
-
+const upgradeCostLimit = 250e12;
+const resource = `Real Estate`;
 const TICK = 3 * 1000;
 
 /** @param {NS} ns **/
 export async function main(ns) {
 	ns.disableLog('sleep');
 
-	let division = ns.corporate.getCorporation().divisions[0];
-	ns.tprint(division);
+	let division = ns.corporation.getCorporation().divisions[0];
 
 	if (!division.cities.includes(city)) {
 		ns.print(`Prepping to buy office space in ${city}.`);
-		while (ns.corporate.getCorporation().funds < ns.corporation.getExpandCityCost()) { await ns.sleep(TICK); }
+		while (ns.corporation.getCorporation().funds < ns.corporation.getExpandCityCost()) { await ns.sleep(TICK); }
 		ns.corporation.expandCity(division.name, city);
 	}
 	ns.print(`Have office space in ${city}.`);
 
 	if (!ns.corporation.hasWarehouse(division.name, city)) {
 		ns.print(`Prepping to buy warehouse in ${city}.`);
-		while (ns.corporate.getCorporation().funds < ns.corporation.getPurchaseWarehouseCost()) { await ns.sleep(TICK); }
+		while (ns.corporation.getCorporation().funds < ns.corporation.getPurchaseWarehouseCost()) { await ns.sleep(TICK); }
 		ns.corporation.purchaseWarehouse(division.name, city);
 	}
 	ns.print(`Have warehouse in ${city}.`);
 
-	while (ns.corporation.getWarehouse(division.name, city).size < warehouseSize) {
-		while (ns.corporate.getCorporation().funds < ns.corporate.getUpgradeWarehouseCost(division.name, city)) { await ns.sleep(TICK); }
-		ns.corporate.upgradeWarehouse(division.name, city);
+	while (ns.corporation.getUpgradeWarehouseCost(division.name, city) < upgradeCostLimit) {
+		while (ns.corporation.getCorporation().funds < ns.corporation.getUpgradeWarehouseCost(division.name, city)) { await ns.sleep(TICK); }
+		ns.corporation.upgradeWarehouse(division.name, city);
+		ns.print(`${city} warehouse size: ${ns.corporation.getWarehouse(division.name, city).size}`);
 	}
 
-	
+	let units = 0.8 * ns.corporation.getWarehouse(division.name, city).size * 200;
+	if (ns.corporation.hasResearched(division.name, `Bulk Purchase`)) {
+		ns.print(`Buying ${units} units of ${resource} the quick and easy way.`);
+		ns.corporation.bulkPurchase(division.name, city, `Real Estate`, units);
+	} else {
+		ns.print(`Buying ${units} units of ${resource} in the clunky way.`);
+		let rate = units / 100; // purchase over 10 market cycles, or around 100 seconds
+		ns.corporation.buyMaterial(division.name, city, resource, rate);
+		while (ns.corporation.getMaterial(division.name, city, resource).qty < units) { await ns.sleep(TICK); }
+		ns.corporation.buyMaterial(division.name, city, resource, 0);
+	}
 
+	ns.tprint(ns.corporation.getOffice(division.name, city));
 }
-/*
+/*  CorporationInfo
 {
 	"name":"CORP",
 	"funds":3.650707433269212e+23,
@@ -75,12 +85,32 @@ export async function main(ns) {
 }
 */
 
-/*
+/*  WarehouseInfo
 {
 	"level":1,
 	"loc":"New Tokyo",
 	"size":100,
 	"sizeUsed":0,
 	"smartSupplyEnabled":true
+}
+*/
+
+/*  OfficeInfo
+{
+	"loc":"Aevum",
+	"size":3,
+	"minEne":0,
+	"maxEne":100,
+	"minHap":0,
+	"maxHap":100,
+	"maxMor":100,
+	"employees":[],
+	"employeeProd":{
+		"Operations":0,
+		"Engineer":0,
+		"Business":0,
+		"Management":0,
+		"Research & Development":0
+	}
 }
 */
